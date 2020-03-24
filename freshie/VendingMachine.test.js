5741b16540739440efd468649b9cdba5dd5b4aea
Valerie Lui <vallq@users.noreply.github.com>
2020-03-24 23:53:03 +0800

    add tests for part 2 of question

--- freshie/VendingMachine.test.js
+++ freshie/VendingMachine.test.js
@@ -1,7 +1,5 @@
 const VendingMachine = require("./VendingMachine");
-// beforeEach(() => {
-//   const Freshie = new VendingMachine();
-// });
+
 describe("Testing", () => {
   describe("Level 0", () => {
     it(`should return ["OJ"] and no change when input is OJ and 200`, () => {
@@ -27,9 +25,23 @@ describe("Testing", () => {
       expect(Freshie.changeToDispense).toEqual(150);
       expect(Freshie.sendOutput()).toEqual(expectedOutput);
     });
+  });
 
+  describe("Level 1", () => {
+    it(`should return ["CW"] and no change when input is CW and 350`, () => {
+      const Freshie = new VendingMachine();
+      Freshie.receiveJuiceTypeAndCash("CW", [50, 100], [200]);
+      const expectedOutput = ["CW"];
+      expect(Freshie.changeToDispense).toEqual(0);
+      expect(Freshie.sendOutput()).toEqual(expectedOutput);
+    });
+
+    it(`should return ["CW", 200, 100, 20, 10] and no change when input is CW and 680`, () => {
+      const Freshie = new VendingMachine();
+      Freshie.receiveJuiceTypeAndCash("CW", [10, 20, 50, 100], [500]);
+      const expectedOutput = ["CW", 200, 100, 20, 10];
+      expect(Freshie.changeToDispense).toEqual(330);
+      expect(Freshie.sendOutput()).toEqual(expectedOutput);
+    });
   });
 });
-
-//   Input: ([10, 20, 50, 100], [500])
-// Output: ["OJ", 200, 200, 50, 20, 10]
